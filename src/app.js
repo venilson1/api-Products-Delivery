f6d6ec563885c4f5cb727b09a24ca7c6eb982ccb
venilson1 <venilson2009@hotmail.com>
2021-04-25 17:27:00 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectDB = require('./database/Connection');
+const router = require("./routers");
 
 const app = express();
+
 connectDB();
 
+app.use(express.json()); 
 
-app.use(express.json());  
+app.use("/",router);
 
 const Port = process.env.Port || 3000;
-app.listen(Port, () => console.log("Servidor rodando"));
+app.listen(Port, () => console.log("Servidor rodando porta 3000"));
