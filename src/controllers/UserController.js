cd076ea4fee93cd0a3e89b820558c7aa29482516
venilson1 <venilson2009@hotmail.com>
2021-04-27 17:00:51 -0300

    validation empty

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,14 +2,21 @@ class UserController {
 
   async index(req, res){
     res.send({
-      name: "Venilson",
+      name: "venilson",
       email: "venilson2009@hotmail.com"
     });
   }
 
   async create(req, res){
     console.log(req.body);
-    res.send(req.body);
+
+    const {name, email, password} =  req.body;
+
+    if (!name) res.status(400).send({err: "O nome está invalido"});
+    if (!email) res.status(400).send({err: "O e-mail está invalido"});
+    if (!password) res.status(400).send({err: "Senha invalida"});
+
+    res.status(200).send(req.body);
   }
 }
 
