9b8b547ed98040fd189dfc4986fc566ca610f9df
venilson1 <venilson2009@hotmail.com>
2021-04-27 22:40:27 -0300

    find Email

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const userServices = require('../services/userServices');
+const bcrypt = require('bcrypt')
 
 class UserController {
 
@@ -9,17 +10,32 @@ class UserController {
     });
   }
 
-  async create(req, res){
-    const {name, email, password} =  req.body;
+  async newUser(req, res){
+    let {name, email, password, role} =  req.body;
 
     if (!name) res.status(400).json({err: "O nome está invalido"});
     if (!email) res.status(400).json({err: "O e-mail está invalido"});
     if (!password) res.status(400).json({err: "Senha invalida"});
+    if (!role) role = 0;
 
-    const status = await userServices.Create(name, email, password);
+    let emailExists =  await userServices.findEmail(email);
 
+      if (emailExists) {
+        res.status(406).json({error: 'Email já Cadastrado'})
+        return;
+      }
 
-    res.status(200).send(status);
+    try 
+    {
+      let hash = await bcrypt.hash(password, 10);
+      const status = await userServices.Create(name, email, password = hash, role);
+      res.status(200).send(status);
+    } 
+
+    catch (error) 
+    {
+      console.log(error);
+    }
   }
 }
 
